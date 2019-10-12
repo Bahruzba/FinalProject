44383185afdf0be5ff0342a59817a0ad29107ea4
Your Name <you@example.com>
2019-10-12 23:24:07 +0400

    wrote offer and listing pages

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$('.owl-carousel').slick({
+$('#Find-awesome-deals .owl-carousel').slick({
     infinite: true,
     slidesToShow: 6,
     slidesToScroll: 1,
@@ -23,6 +23,55 @@ $('.owl-carousel').slick({
     ]
 });
 
+$('#popular-brands .owl-carousel').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: false,
+    nextArrow: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    responsive: [
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 5,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 3,
+            }
+        }
+    ]
+});
+
+$('#offers .owl-carousel').slick({
+    infinite: true,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    prevArrow: false,
+    nextArrow: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    infinite: true,
+    responsive: [
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 5,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 3,
+            }
+        }
+    ]
+});
 
 $(document).ready(function () {
     
@@ -32,9 +81,9 @@ $(".form form input").on("input", function(){
         $(this).prev().animate({top:x+2+"px",fontSize:"12px", opacity:"1"},150);
         return;
     }
-    if($(this).val()==""){
-        return;
-    }
+    // if($(this).val()==""){
+    //     return;
+    // }
     $(this).prev().animate({top:x-12+"px",fontSize:"10px", opacity:"0.7"},150);
 });
 
@@ -46,7 +95,21 @@ $(".form form input").focus(function(){
     $(".form form input").css("box-shadow", "none");
 });
 
+$(document).on("click", "#offers .item .header-tab-menu", function(){
+    var heightItem=$(this).next().height()+81;
+    if($(this).parent().height()>70){
+        $(this).parent().animate({height: "65px"});
+        return;
+    }
+    $("#offers .filters .item").animate({height: "65px"});
+    $(this).parents(".item").animate({height: heightItem});
+})
 
+$(document).on("click", "#offers .item .body-tab-menu i", function(){
+    $(this).toggleClass("select");
+    $("#offers .item .body-tab-menu i").addClass("")
+
+})
 
 
 
